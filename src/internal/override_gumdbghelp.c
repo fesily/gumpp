5066b261fd986b0b7846e6dd81b182bc6af84478
fesily <fesil@foxmail.com>
2023-02-28 09:37:37 +0800

    use system dbghelp

---
+++ src/internal/override_gumdbghelp.c
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2008-2022 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2020 Matt Oh <oh.jeongwook@gmail.com>
+ *
+ * Licence: wxWindows Library Licence, Version 3.1
+ */
+#ifdef _WIN32
+#include "frida-gum.h"
+typedef void (*GumDestructorFunc) (void);
+GUM_API void _gum_register_destructor(GumDestructorFunc destructor);
+
+#include <Windows.h>
+#include <DbgHelp.h>
+#pragma comment(lib, "Dbghelp.lib")
+struct _GumDbghelpImplPrivate
+{
+	HMODULE module;
+};
+
+static gpointer do_init(gpointer data);
+static void do_deinit(void);
+
+static HMODULE load_dbghelp(void);
+
+static void gum_dbghelp_impl_lock(void);
+static void gum_dbghelp_impl_unlock(void);
+
+#define INIT_IMPL_FUNC(func) \
+    *((gpointer *) (&impl->func)) = \
+        GSIZE_TO_POINTER (GetProcAddress (mod, G_STRINGIFY (func))); \
+    g_assert (impl->func != NULL)
+
+typedef struct _GumDbghelpImpl        GumDbghelpImpl;
+typedef struct _GumDbghelpImplPrivate GumDbghelpImplPrivate;
+
+struct _GumDbghelpImpl
+{
+	BOOL(WINAPI* StackWalk64) (DWORD MachineType, HANDLE hProcess,
+		HANDLE hThread, LPSTACKFRAME64 StackFrame, PVOID ContextRecord,
+		PREAD_PROCESS_MEMORY_ROUTINE64 ReadMemoryRoutine,
+		PFUNCTION_TABLE_ACCESS_ROUTINE64 FunctionTableAccessRoutine,
+		PGET_MODULE_BASE_ROUTINE64 GetModuleBaseRoutine,
+		PTRANSLATE_ADDRESS_ROUTINE64 TranslateAddress);
+	DWORD(WINAPI* SymSetOptions) (DWORD SymOptions);
+	BOOL(WINAPI* SymInitialize) (HANDLE hProcess, PCSTR UserSearchPath,
+		BOOL fInvadeProcess);
+	BOOL(WINAPI* SymCleanup) (HANDLE hProcess);
+	BOOL(WINAPI* SymEnumSymbols) (HANDLE hProcess, ULONG64 BaseOfDll,
+		PCSTR Mask, PSYM_ENUMERATESYMBOLS_CALLBACK EnumSymbolsCallback,
+		PVOID UserContext);
+	BOOL(WINAPI* SymFromAddr) (HANDLE hProcess, DWORD64 Address,
+		PDWORD64 Displacement, PSYMBOL_INFO Symbol);
+	PVOID(WINAPI* SymFunctionTableAccess64) (HANDLE hProcess,
+		DWORD64 AddrBase);
+	BOOL(WINAPI* SymGetLineFromAddr64) (HANDLE hProcess, DWORD64 qwAddr,
+		PDWORD pdwDisplacement, PIMAGEHLP_LINE64 Line64);
+	DWORD64(WINAPI* SymLoadModuleExW) (HANDLE hProcess, HANDLE hFile,
+		PCWSTR ImageName, PCWSTR ModuleName, DWORD64 BaseOfDll, DWORD DllSize,
+		PMODLOAD_DATA Data, DWORD Flags);
+	DWORD64(WINAPI* SymGetModuleBase64) (HANDLE hProcess, DWORD64 qwAddr);
+	BOOL(WINAPI* SymGetModuleInfo) (HANDLE hProcess, DWORD dwAddr,
+		PIMAGEHLP_MODULE ModuleInfo);
+	BOOL(WINAPI* SymGetTypeInfo) (HANDLE hProcess, DWORD64 ModBase,
+		ULONG TypeId, IMAGEHLP_SYMBOL_TYPE_INFO GetType, PVOID pInfo);
+
+	void (*Lock) (void);
+	void (*Unlock) (void);
+
+	/*< private */
+	GumDbghelpImplPrivate* priv;
+};
+
+GumDbghelpImpl*
+gum_dbghelp_impl_try_obtain(void)
+{
+	static GOnce init_once = G_ONCE_INIT;
+
+	g_once(&init_once, do_init, NULL);
+
+	return init_once.retval;
+}
+
+static gpointer
+do_init(gpointer data)
+{
+	HMODULE mod;
+	GumDbghelpImpl* impl;
+
+	mod = load_dbghelp();
+	if (mod == NULL)
+		return NULL;
+
+	impl = g_slice_new0(GumDbghelpImpl);
+	impl->priv = g_slice_new(GumDbghelpImplPrivate);
+	impl->priv->module = mod;
+
+	INIT_IMPL_FUNC(StackWalk64);
+	INIT_IMPL_FUNC(SymSetOptions);
+	INIT_IMPL_FUNC(SymInitialize);
+	INIT_IMPL_FUNC(SymCleanup);
+	INIT_IMPL_FUNC(SymEnumSymbols);
+	INIT_IMPL_FUNC(SymFromAddr);
+	INIT_IMPL_FUNC(SymFunctionTableAccess64);
+	INIT_IMPL_FUNC(SymGetLineFromAddr64);
+	INIT_IMPL_FUNC(SymLoadModuleExW);
+	INIT_IMPL_FUNC(SymGetModuleBase64);
+	INIT_IMPL_FUNC(SymGetModuleInfo);
+	INIT_IMPL_FUNC(SymGetTypeInfo);
+
+	impl->Lock = gum_dbghelp_impl_lock;
+	impl->Unlock = gum_dbghelp_impl_unlock;
+
+	impl->SymInitialize(GetCurrentProcess(), NULL, TRUE);
+
+	_gum_register_destructor(do_deinit);
+
+	return impl;
+}
+
+static void
+do_deinit(void)
+{
+	GumDbghelpImpl* impl;
+
+	impl = gum_dbghelp_impl_try_obtain();
+	g_assert(impl != NULL);
+
+	impl->SymCleanup(GetCurrentProcess());
+
+	FreeLibrary(impl->priv->module);
+	g_slice_free(GumDbghelpImplPrivate, impl->priv);
+	g_slice_free(GumDbghelpImpl, impl);
+}
+
+static HMODULE
+load_dbghelp(void)
+{
+	HMODULE mod;
+    if (GetModuleHandleExW(0, L"dbghelp.dll", &mod)) {
+        return mod;
+    }
+	BOOL success G_GNUC_UNUSED;
+	DWORD length G_GNUC_UNUSED;
+	WCHAR path[MAX_PATH + 1] = { 0, };
+	WCHAR* filename;
+
+	success = GetModuleHandleExW(
+		GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+		GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+		GUM_FUNCPTR_TO_POINTER(load_dbghelp),
+		&mod);
+	g_assert(success);
+
+	length = GetModuleFileNameW(mod, path, MAX_PATH);
+	g_assert(length != 0);
+
+	filename = wcsrchr(path, L'\\');
+	g_assert(filename != NULL);
+	filename++;
+	wcscpy(filename, L"dbghelp.dll");
+
+	return LoadLibraryW(path);
+}
+
+static GMutex _gum_dbghelp_mutex;
+
+static void
+gum_dbghelp_impl_lock(void)
+{
+	g_mutex_lock(&_gum_dbghelp_mutex);
+}
+
+static void
+gum_dbghelp_impl_unlock(void)
+{
+	g_mutex_unlock(&_gum_dbghelp_mutex);
+}
+#endif
--- src/runtime.cpp
+++ src/runtime.cpp
@@ -2,68 +2,134 @@
 
 #include <frida-gum.h>
 #ifdef HAVE_WINDOWS
+#include <Dbghelp.h>
 #include <windows.h>
+#include <cstdio>
+#include <filesystem>
 #endif
 
-namespace Gum
-{
-  volatile int Runtime::ref_count = 0;
+namespace Gum {
+volatile int Runtime::ref_count = 0;
+#ifdef _WIN32
+struct _GumDbghelpImpl;
+extern "C" _GumDbghelpImpl* gum_dbghelp_impl_try_obtain(void);
+inline std::string searchpath(bool SymBuildPath, bool SymUseSymSrv) {
+  // Build the sym-path:
+  if (SymBuildPath) {
+    std::string searchpath;
+    searchpath.reserve(4096);
+    searchpath.append(".;");
+    std::error_code ec;
+    auto current_path = std::filesystem::current_path(ec);
+    if (!ec) {
+      searchpath.append(current_path.string().c_str());
+      searchpath += ';';
+    }
+    const size_t nTempLen = 1024;
+    char szTemp[nTempLen];
 
-  static void init ()
-  {
-    gum_init_embedded ();
-  }
+    // Now add the path for the main-module:
+    if (GetModuleFileNameA(NULL, szTemp, nTempLen) > 0) {
+      std::filesystem::path path(szTemp);
+      searchpath.append(path.parent_path().string());
+      searchpath += ';';
+    }
+    if (GetEnvironmentVariableA("_NT_SYMBOL_PATH", szTemp, nTempLen) > 0) {
+      szTemp[nTempLen - 1] = 0;
+      searchpath.append(szTemp);
+      searchpath += ';';
+    }
+    if (GetEnvironmentVariableA("_NT_ALTERNATE_SYMBOL_PATH", szTemp, nTempLen) >
+        0) {
+      szTemp[nTempLen - 1] = 0;
+      searchpath.append(szTemp);
+      searchpath += ';';
+    }
+    if (GetEnvironmentVariableA("SYSTEMROOT", szTemp, nTempLen) > 0) {
+      szTemp[nTempLen - 1] = 0;
+      searchpath.append(szTemp);
+      searchpath += ';';
+      searchpath.append(szTemp);
+      searchpath.append("\\system32;");
+    }
 
-  static void deinit ()
-  {
-    gum_deinit_embedded ();
-  }
+    if (SymUseSymSrv) {
+      if (GetEnvironmentVariableA("SYSTEMDRIVE", szTemp, nTempLen) > 0) {
+        szTemp[nTempLen - 1] = 0;
+        searchpath.append("SRV*");
+        searchpath.append(szTemp);
+        searchpath.append(
+            "\\websymbols*https://msdl.microsoft.com/download/symbols;");
+      } else
+        searchpath.append(
+            "SRV*c:\\websymbols*https://msdl.microsoft.com/download/symbols;");
+    }
+    return searchpath;
+  }  // if SymBuildPath
+  return {};
+}
+void init_gdbhelp() {
+  gum_dbghelp_impl_try_obtain();
+  HANDLE proc = GetCurrentProcess();
+#ifndef NDEBUG
+  char oldpath[2048];
+  SymGetSearchPath(proc, oldpath, sizeof(oldpath));
+  printf("SymGetSearchPath:%s", oldpath);
+#endif
+  SymSetSearchPath(proc, searchpath(true, true).c_str());
+}
+#else
+void init_gdbhelp() {}
+#endif
+
+static void init() {
+  gum_init_embedded();
+  init_gdbhelp();
+}
+
+static void deinit() {
+  gum_deinit_embedded();
+}
 
 #if defined(HAVE_WINDOWS) && !defined(GUMPP_STATIC)
 
-  extern "C" BOOL WINAPI DllMain (HINSTANCE inst_dll, DWORD reason, LPVOID reserved)
-  {
-    switch (reason)
-    {
-      case DLL_PROCESS_ATTACH:
-        init ();
-        break;
-      case DLL_PROCESS_DETACH:
-        if (reserved == NULL)
-          deinit ();
-        break;
-    }
-
-    return TRUE;
+extern "C" BOOL WINAPI DllMain(HINSTANCE inst_dll,
+                               DWORD reason,
+                               LPVOID reserved) {
+  switch (reason) {
+    case DLL_PROCESS_ATTACH:
+      init();
+      break;
+    case DLL_PROCESS_DETACH:
+      if (reserved == NULL)
+        deinit();
+      break;
   }
 
-  void Runtime::ref ()
-  {
-  }
+  return TRUE;
+}
 
-  void Runtime::unref ()
-  {
-  }
+void Runtime::ref() {}
+
+void Runtime::unref() {}
 
 #else
 
-  void Runtime::ref ()
-  {
-    if (g_atomic_int_add (&ref_count, 1) == 0)
-      init ();
-  }
+void Runtime::ref() {
+  if (g_atomic_int_add(&ref_count, 1) == 0)
+    init();
+}
 
-  void Runtime::unref ()
-  {
-    if (g_atomic_int_dec_and_test (&ref_count))
-      deinit ();
-  }
+void Runtime::unref() {
+  if (g_atomic_int_dec_and_test(&ref_count))
+    deinit();
+}
 #endif
 
-  void runtime_init(){
-    Runtime::ref();
-  }
-  void runtime_deinit(){
-    Runtime::unref();
-  }
+void runtime_init() {
+  Runtime::ref();
 }
+void runtime_deinit() {
+  Runtime::unref();
+}
+}  // namespace Gum
